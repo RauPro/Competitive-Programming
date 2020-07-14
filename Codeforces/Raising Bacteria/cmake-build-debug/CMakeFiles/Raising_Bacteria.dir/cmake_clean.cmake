9840c674bf9d540ca2ff819558814108b5643dd6
RauPro <00012119@uca.edu.sv>
2020-07-13 21:21:03 -0600

    Added: Problem2

---
+++ Codeforces/Raising Bacteria/cmake-build-debug/CMakeFiles/Raising_Bacteria.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Raising_Bacteria.dir/main.cpp.obj"
+  "Raising_Bacteria.exe"
+  "Raising_Bacteria.exe.manifest"
+  "Raising_Bacteria.pdb"
+  "libRaising_Bacteria.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Raising_Bacteria.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
