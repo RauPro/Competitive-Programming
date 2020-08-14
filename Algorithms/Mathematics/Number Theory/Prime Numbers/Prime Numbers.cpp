9a3bc6dfc9600c49f7ae87a93cf0e9e651d6af03
RauPro <00012119@uca.edu.sv>
2020-08-14 16:12:38 -0600

    Added: Some Algorithms for prime numbers

---
+++ Algorithms/Mathematics/Number Theory/Prime Numbers/Prime Numbers.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Fast ios::sync_with_stdio(0); cin.tie(0);
+
+typedef long long ll;
+typedef long int li;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+
+//count primes
+int count_primes(int n) {
+    const int S = 10000;
+
+    vector<int> primes;
+    int nsqrt = sqrt(n);
+    vector<char> is_prime(nsqrt + 1, true);
+    for (int i = 2; i <= nsqrt; i++) {
+        if (is_prime[i]) {
+            primes.push_back(i);
+            for (int j = i * i; j <= nsqrt; j += i)
+                is_prime[j] = false;
+        }
+    }
+
+    int result = 0;
+    vector<char> block(S);
+    for (int k = 0; k * S <= n; k++) {
+        fill(block.begin(), block.end(), true);
+        int start = k * S;
+        for (int p : primes) {
+            int start_idx = (start + p - 1) / p;
+            int j = max(start_idx, p) * p - start;
+            for (; j < S; j += p)
+                block[j] = false;
+        }
+        if (k == 0)
+            block[0] = block[1] = false;
+        for (int i = 0; i < S && start + i <= n; i++) {
+            if (block[i])
+                result++;
+        }
+    }
+    return result;
+}
+
+//Find primes in range
+vector<bool> segmentedSieve(long long L, long long R) {
+    // generate all primes up to sqrt(R)
+    long long lim = sqrt(R);
+    vector<bool> mark(lim + 1, false);
+    vector<long long> primes;
+    for (long long i = 2; i <= lim; ++i) {
+        if (!mark[i]) {
+            primes.push_back(i);
+            for (long long j = i * i; j <= lim; j += i)
+                mark[j] = true;
+        }
+    }
+
+    vector<bool> isPrime(R - L + 1, true);
+    for (long long i : primes)
+        for (long long j = max(i * i, (L + i - 1) / i * i); j <= R; j += i)
+            isPrime[j - L] = false;
+    if (L == 1)
+        isPrime[0] = false;
+    return isPrime;
+}
+//Excercice A - Almost Prime Codeforces 
+int main(){
+	Fast
+	int n;cin>>n;
+	vector<bool> segmentedSiev= segmentedSieve(0,n);
+	for(int i=2;i<segmentedSiev.size();i++){
+		if(segmentedSiev[i]==true){
+			cout<<i<<endl;
+		}
+	}
+} 
