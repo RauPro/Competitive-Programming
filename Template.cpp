2b9b62f05b86b7540d4bef9566332909d6b5bd42
RauPro <00012119@uca.edu.sv>
2021-07-21 20:45:52 -0600

    Update template

--- Template.cpp
+++ Template.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 using namespace __gnu_pbds;
 
+#define LSOne(S) ((S) & -(S)) // Useful bitmask operation
 #define Fast ios::sync_with_stdio(0); cin.tie(0);
 
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ost;
@@ -11,10 +12,21 @@ typedef long long ll;
 typedef long int li;
 typedef vector<int> vi;
 typedef vector<ll> vl;
+typedef vector<ll> vll;
 typedef pair<int, int> pi;
+
+class FastScanner{
+public:
+	int next(){int a;cin>>a;return a;}
+	vi readArray(int n){
+		vi a(n);
+		for (size_t i = 0; i < n; i++)cin>>a[i];
+		return a;}
+};
 
 
 int main(){
 	Fast
+	FastScanner fs;
 	return 0;
 } 
