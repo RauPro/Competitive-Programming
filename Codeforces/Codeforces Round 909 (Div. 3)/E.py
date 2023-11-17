47231ed06426c57a0706c8f9b6529feb255c5a71
Raul Ernesto Guillen Hernandez <00012119@uca.edu.sv>
2023-11-17 11:08:39 -0600

    template

--- Codeforces/Codeforces Round 909 (Div. 3)/E.py
+++ Codeforces/Codeforces Round 909 (Div. 3)/E.py
@@ -1,32 +1,85 @@
-def min_operations_to_sort(t, test_cases):
-    results = []
+import os
+import sys
+from collections import *
+from heapq import *
+from math import gcd, floor, ceil, sqrt
+from copy import deepcopy
+from itertools import permutations, combinations, product
+from bisect import bisect_left, bisect_right
+from functools import lru_cache, reduce
+import operator
 
-    for n, arr in test_cases:
-        if sorted(arr) == arr:
-            results.append(0)
-            continue
+# Para mejorar el rendimiento de la entrada/salida
+input = lambda: sys.stdin.readline().strip()
+flush = lambda: sys.stdout.flush()
+print = lambda *args, **kwargs: sys.stdout.write(' '.join(map(str, args)) + kwargs.get("end", "\n")) and flush()
 
-        sorted_index = None
-        for i in range(n - 1, 0, -1):
-            if arr[i] < arr[i - 1]:
-                sorted_index = i
-                break
+# Optimización de la recursión para Python
+sys.setrecursionlimit(100000)
 
-        if sorted_index is None:
-            results.append(-1)
-        else:
-            results.append(sorted_index)
 
-    return results
+# Funciones para lectura de múltiples tipos de datos
+def ints(): return map(int, input().split())
+def strs(): return input().split()
+def chars(): return list(input().strip())
+def mat(n): return [list(ints()) for _ in range(n)]  # Matriz de n x m donde m es el número de enteros en una línea
 
-# Ejemplo de uso
-t = 5
-test_cases = [
-    (5, [6, 4, 1, 2, 5]),
-    (7, [4, 5, 3, 7, 8, 6, 2]),
-    (6, [4, 3, 1, 2, 6, 4]),
-    (4, [5, 2, 4, 2]),
-    (3, [2, 2, 3])
-]
 
-print(min_operations_to_sort(t, test_cases))
+# Constantes útiles
+INF = float('inf')
+MOD = 1000000007  # Modulo por defecto, cambiar si se necesita otro
+
+
+# Algunas funciones útiles
+def add(x, y, mod=MOD): return (x + y) % mod
+def sub(x, y, mod=MOD): return (x - y) % mod
+def mul(x, y, mod=MOD): return (x * y) % mod
+
+
+# Fast power - a^b % mod
+def powmod(a, b, mod=MOD):
+    res = 1
+    a = a % mod
+    while b > 0:
+        if b % 2:
+            res = mul(res, a, mod)
+        a = mul(a, a, mod)
+        b //= 2
+    return res
+
+
+# Inverso multiplicativo de a modulo m (cuando m es primo)
+def invmod(a, mod=MOD): return powmod(a, mod - 2, mod)
+
+
+# GCD y LCM
+def lcm(a, b): return a * b // gcd(a, b)
+
+
+# Factorial con memoización
+@lru_cache(maxsize=None)
+def factorial(n): return n * factorial(n - 1) if n else 1
+
+
+# Combinaciones con memoización (nCr)
+@lru_cache(maxsize=None)
+def comb(n, r):
+    if r == 0 or r == n: return 1
+    return comb(n - 1, r - 1) + comb(n - 1, r)
+
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(ints())
+        print(solve(n, a))
+
+
+def solve():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
